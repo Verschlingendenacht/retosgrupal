8a03e6cb5ed43144d9b1ac43b12935420aeaaaeb
Verschlingendenacht <nilzon.gomez02@gmail.com>
2025-06-16 17:21:08 -0500

    LeetCode 2

---
+++ Java/Nilzon/o2.java
@@ -0,0 +1,30 @@
+package Java.Nilzon;
+
+/*
+* Objetivo: encontrar la resta mas grande posible de un arreglo dado
+* Condiciones de la resta a retornar:
+* el indice del primer operando debe ser menor al segundo operando (osea, debe de encontrarse en orden de izquierda a derecha cada pareja)
+* el valor del primer indice debe ser menor al valor del segundo indice
+* */
+
+class o2 {
+
+    public static void main(String[] args){
+
+        System.out.println(maximumDifference(new int[]{1,5,2,10}));
+    }
+
+    static public int maximumDifference(int[] nums) {
+        int min = nums[0]; //dada la condicion, asumiremos que el primer numero es el menor
+        int maxDiff = -1; //de esta forma retornamos directamente -1 (como pide el problema) sin importarnos que suceda
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > min) {
+                maxDiff = Math.max(maxDiff, nums[i] - min); //maxDiff representa la resta anterior o -1, si la resta actual es mayor entonces hemos encontrado el objetivo
+            } else {
+                min = nums[i]; //si no se encontraron parejas donde min sea el menor, probamos con el siguiente min
+            }
+        }
+        return maxDiff;
+    }
+}
